785ea7102924168c9428f9b9102768e3302c970f
Hiruna Saranga Bandara <hiruna12saranga12@gmail.com>
2025-03-22 12:22:21 +0530

    User Info Display
    This code is used to display the current user's username and the Windows version in the Command Prompt. It also pauses to allow the user to see the output before the window closes.

---
+++ Day 01_2025-03-21/CMD_Commands/Codes/PrintDetails.bat
@@ -0,0 +1,12 @@
+@echo off
+:: Turn off the command display in the terminal.
+
+echo Username : %USERNAME%
+:: echo command used for print text on the command window.
+:: %USERNAME% used for display the username of the user.
+
+echo Version : 
+:: ver command is used to display current Windows version.
+
+pause
+:: is used for stop the script and wait for user input(press any key).
